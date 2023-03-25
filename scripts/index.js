6a7e34c2d8518f186553d306f633f7f71ff51922
Harieta Hubanova <harieta5hubanova@gmail.com>
2023-03-25 16:46:48 +0000

    Hard win

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
 console.log('JS Loaded!');
 const canvas = document.getElementById('hell');
 const ctx = canvas.getContext('2d');
-const img = new Image();
-img.src = '../image/initial-game-page.jpeg';
 const gamePage = new Image();
 gamePage.src = '../image/game-page.jpeg';
+const background = new Image();
 
 function getRandomMovie(bestMovies) {
   const randomIndex = Math.floor(Math.random() * bestMovies.length);
 
   const movie = bestMovies[randomIndex];
+  background.src = movie.src;
 
-  return movie;
+  return movie.title;
 }
 // let dashedName = getRandomMovie(bestMovies);
 // console.log(dashedName.split);
@@ -32,14 +32,19 @@ let dashedName = updateName();
 let numberOfGuesses = 5;
 
 function updateCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(gamePage, 0, 0, gamePage.width, gamePage.height);
+  if (!dashedName.split('').includes('-')) {
+    console.log('here');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0, 1500, 700);
+  } else {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(gamePage, 0, 0, gamePage.width, gamePage.height);
+    ctx.font = '60px Arial';
 
-  ctx.font = '80px Arial';
-
-  ctx.fillText(dashedName, 450, 300);
-  ctx.font = '50px Arial';
-  ctx.fillText(`Guesses: ${numberOfGuesses}`, 1000, 50);
+    ctx.fillText(dashedName, 450, 300);
+    ctx.font = '50px Arial';
+    ctx.fillText(`Guesses: ${numberOfGuesses}`, 1000, 50);
+  }
 }
 
 function actualGameBackground() {
@@ -87,7 +92,6 @@ document.addEventListener('keydown', event => {
       updateCanvas();
     }
   }
-
   gameOver();
 });
 function gameOver() {
@@ -97,3 +101,17 @@ function gameOver() {
     ctx.fillText('GAME OVER', 500, 300);
   }
 }
+function gameWon() {
+  if (!dashedName.split('').includes('-')) {
+    background.src = randomTitle.src;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0, 1500, 700);
+  }
+}
+
+//function gameWon() {
+//if (randomTitle === 'pulp   fiction') {
+// ctx.clearRect(0, 0, canvas.width, canvas.length);
+// ctx.drawImage((src = '/image/PulpFiction.jpeg'), 0, 0, 1500, 700);
+//}
+//}
--- scripts/movies.js
+++ scripts/movies.js
@@ -1,12 +1,24 @@
 const bestMovies = [
-  'there   will   be  blood',
-  'beau   travail',
-  'pulp   fiction',
-  'the   truman   show',
-  'lost   in   translation',
-  'night   of   the   living dead',
-  'mad   max   fury   road',
-  'brokeback   mountain',
-  'the   blair   witch   project',
-  'eternal   sunshine   of   the   spotles   mind'
+  {
+    title: 'there   will   be  blood',
+    src: '../image/there will be blood.jpg'
+  },
+  { title: 'beau   travail', src: '../image/Beau Travail.jpg' },
+  { title: 'pulp   fiction', src: '../image/PulpFiction.jpeg' },
+  { title: 'the   truman   show', src: '../image/TheTrumanShow.jpeg' },
+  { title: 'lost   in   translation', src: '../image/LostInTranslation.jpeg' },
+  {
+    title: 'night   of   the   living   dead',
+    src: '../image/nightOfTheLivingDead.jpeg'
+  },
+  { title: 'mad   max   fury   road', src: '../image/MadMaxFury.jpeg' },
+  { title: 'brokeback   mountain', src: '../image/Brokeback Mountain.jpeg' },
+  {
+    title: 'the   blair   witch   project',
+    src: '../image/the blair witch project.jpg'
+  },
+  {
+    title: 'eternal   sunshine   of   the   spotles   mind',
+    src: '../image/eternalSunshineOfTheSpotlessMind.jpeg'
+  }
 ];
